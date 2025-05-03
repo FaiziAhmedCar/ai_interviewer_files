b515ce7b5218bfceeb24e61563f0a4592a1064e5
FaiziAhmedCar <faiziahmedcar@gmail.com>
2025-05-03 11:10:48 +0530

    feat: enhance POST route for interview question generation with improved error handling and techstack processing

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -11,14 +11,20 @@ export async function GET() {
 
 export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
+  if (!role || !type || !level || !userid || !amount) {
+    return Response.json(
+      { success: false, error: "Missing required fields" },
+      { status: 400 }
+    );
+  }
   try {
     const { text: questions } = await generateText({
-      model: google("gemini-2.5-flash-04-17"),
+      model: google("models/gemini-2.0-flash-exp"),
       prompt: `Prepare questions for a job interview.
         The job role is ${role}.
         The job experience level is ${level}.
         The tech stack used in the job is: ${techstack}.
-        The focus between behavioural and technical questions should lean towards: ${type}.
+        The focus between behavioral and technical questions should lean towards: ${type}.
         The amount of questions required is: ${amount}.
         Please return only the questions, without any additional text.
         The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
@@ -28,27 +34,25 @@ export async function POST(request: Request) {
         Thank you! <3
     `,
     });
+
     const interview = {
-      role,
-      type,
-      level,
-      techstack: techstack.split(","),
+      role: role,
+      type: type,
+      level: level,
+      techstack: typeof techstack === "string" ? techstack.split(",") : [],
       questions: JSON.parse(questions),
       userId: userid,
       finalized: true,
       coverImage: getRandomInterviewCover(),
       createdAt: new Date().toISOString(),
     };
+
+
     await db.collection("interviews").add(interview);
-    return Response.json(
-      { success: true, data: interview },
-      { status: 200 }
-    ); 
+
+    return Response.json({ success: true }, { status: 200 });
   } catch (error) {
-    console.log(error);
-    return Response.json(
-      { success: false, error: "Something went wrong" },
-      { status: 500 }
-    );
+    console.error("Error:", error);
+    return Response.json({ success: false, error: error }, { status: 500 });
   }
 }
